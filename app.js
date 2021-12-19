a1863ea14d2b971815bf69ea5fe315cc78f26669
mpower17 <maxim.zaykov97@gmail.com>
2021-12-19 13:45:09 +0300

    добавляет новый эндпойнт

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ import express from "express";
 import sharp from "sharp";
 import imageSize from "image-size";
 import multer from "multer";
+import nodersa from "node-rsa";
+import bodyParser from "body-parser";
+import fs from "fs";
 
 const img = multer({dest: "./img"});
+
+const storage = multer.diskStorage({
+  destination: './upload',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const CORS = {
   'Access-Control-Allow-Origin': '*',
@@ -11,6 +23,10 @@ const CORS = {
   'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Accept, Authorization'
 };
 const app = express();
+
+app.use(express.static('./uploads'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 function login(req, res) {
     res.set(CORS);
@@ -38,7 +54,22 @@ app.post("/size2json", img.single("image"), async (req, res) => {
               height: dimensions.height
           });
   });
-})
+});
+
+app.post( "/decypher/", upload.fields([{name: "secret", maxCount: 1 }, {name: "key", maxCount: 1}]), (req, res, next) => {
+  const files = req.files;
+
+  if (!files) {
+    const error = new Error("Please choose files");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+
+  const privateKey = fs.readFileSync("./upload/key", "utf8");
+  const decrypted = new nodersa(privateKey).decrypt(fs.readFileSync("./upload/secret"),"utf8");
+
+  res.send(decrypted);
+});
 
 app.all('*', login)
 
